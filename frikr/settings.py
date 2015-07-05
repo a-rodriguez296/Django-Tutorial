65eb17f72949944042a2df373e55672116cd6f7f
alejo80805 <alejo80805@hotmail.com>
2015-07-05 13:12:48 -0500

    Log de las sentencias SQL

--- frikr/settings.py
+++ frikr/settings.py
@@ -102,3 +102,20 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# Logs all SQL queries in debug
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        }
+    },
+    'loggers': {
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    }
+}
