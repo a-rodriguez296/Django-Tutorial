3edf012e281cc48407325c4b052752e26647a844
alejo80805 <alejo80805@hotmail.com>
2015-07-05 21:18:49 -0500

    el login ya funciona

--- users/views.py
+++ users/views.py
@@ -1,11 +1,36 @@
+#-*- coding: utf-8 -*-
+
 from django.shortcuts import render, redirect
-from django.contrib.auth import logout as django_logout
+from django.contrib.auth import logout as django_logout, authenticate, login as django_login
 
 # Create your views here.
 
 
 def login(request):
-    return render(request, 'users/login.html')
+
+    error_messages = []
+
+    if request.method == 'POST':
+
+        #forma segura de acceder a claves en un diccionario. Nunca acceder a un diccionario con la sintaxis de corchetes. POST['usr']
+        username = request.POST.get('usr')
+        password = request.POST.get('pwd')
+
+        user = authenticate (username = username, password = password)
+
+        if user is None:
+            error_messages.append('Nombre de usuario o contraseña incorrecto')
+        else:
+            if user.is_active:
+                django_login(request, user)
+                return redirect('photos_home')
+            else:
+                error_messages.append('El usuario no esta activo')
+
+    context = {
+        'errors' : error_messages
+    }
+    return render(request, 'users/login.html', context)
 
 
 def logout(request):
