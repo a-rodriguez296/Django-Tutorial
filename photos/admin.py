536e85d7091385086f835f4a03fab23f3f29dab0
alejo80805 <alejo80805@hotmail.com>
2015-07-08 15:31:34 -0500

    mejorando el administrador

--- photos/admin.py
+++ photos/admin.py
@@ -1,6 +1,41 @@
 from django.contrib import admin
 from photos.models import Photo
 
-# Register your models here.
 
-admin.site.register(Photo)
+class PhotoAdmin(admin.ModelAdmin):
+
+
+
+    list_display = ('name', 'owner_name', 'license', 'visibility')
+    list_filter = ('license', 'visibility')
+    search_fields = ('name', 'description')
+
+    def owner_name(self, obj):
+        return obj.owner.first_name + u' ' + obj.owner.last_name
+
+    owner_name.short_description = u'Photo owner'
+
+    #esto no se para que sirve
+    owner_name.admin_order_field = 'owner'
+
+    fieldsets = (
+
+        (None, {
+            'fields': ('name',),
+            'classes': ('wide',),
+        }),
+        ('Description & Author', {
+            'fields': ('description', 'owner'),
+            'classes': ('wide',)
+        }),
+        ('Extra', {
+            'fields':('url', 'license', 'visibility'),
+            'classes' : ('wide', 'collapse')
+        })
+
+    )
+
+
+
+#Esto quiere decir que la entidad Photo es administrada por PhotoAdmin
+admin.site.register(Photo, PhotoAdmin)
