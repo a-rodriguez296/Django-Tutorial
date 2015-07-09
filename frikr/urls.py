b50508f5086a4a3085712807ef976f0ca5e5a3db
alejo80805 <alejo80805@hotmail.com>
2015-07-09 09:49:02 -0500

    end-point detalle usuario

--- frikr/urls.py
+++ frikr/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from photos.views import HomeView, DetailView, CreateView, PhotoListView, UserPhotosView
 from users.views import LoginView, LogoutView
-from users.api import UserListAPI
+from users.api import UserListAPI, UserDetailApi
 from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
@@ -41,6 +41,7 @@ urlpatterns = [
     url(r'^logout$', LogoutView.as_view(), name='users_logout'),
 
     #Users API URL's
-    url(r'^api/1.0/users/', UserListAPI.as_view(), name='users_list_api'),
+    url(r'^api/1.0/users/$', UserListAPI.as_view(), name='users_list_api'),
+    url(r'^api/1.0/users/(?P<pk>[0-9]+)$', UserDetailApi.as_view(), name='user_detail_api'),
 
 ]
--- users/api.py
+++ users/api.py
@@ -6,6 +6,7 @@ from django.contrib.auth.models import User
 from users.serializers import UserSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 
 
 class UserListAPI(APIView):
@@ -17,3 +18,13 @@ class UserListAPI(APIView):
         serializer = UserSerializer(users, many=True)
 
         return Response(serializer.data)
+
+
+class UserDetailApi(APIView):
+
+    #pq es la varialbe que me mandan desde urls
+    def get(self, request, pk):
+        user = get_object_or_404(User, pk=pk)
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+
