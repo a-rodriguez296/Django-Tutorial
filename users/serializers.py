12d642d590fc2bff58e83e6f03e31e34ca91df01
alejo80805 <alejo80805@hotmail.com>
2015-07-09 12:41:06 -0500

    endpoint actualizar no funciona borrar si funciona

--- users/serializers.py
+++ users/serializers.py
@@ -24,7 +24,7 @@ class UserSerializer(serializers.Serializer):
         instance.first_name = validated_data.get('first_name')
         instance.last_name = validated_data.get('last_name')
         instance.username = validated_data.get('username')
-
+        instance.email = validated_data.get('email')
         #Esto se hace para encriptar la contraseña
         instance.set_password(validated_data.get('password'))
 
@@ -33,11 +33,16 @@ class UserSerializer(serializers.Serializer):
 
         return instance
 
-    #como se usa validate+campo en data ya viene el valor. Si solo fuera validate vendría todo en un diccionario.
+    #como se usa validate+campo en data ya viene el valor. Si solo fuera validate vendría  to do en un diccionario.
     def validate_username(self, data):
 
         users = User.objects.filter(username=data)
-        if len(users) > 0:
+
+        #Si estoy creando un usuario (self.instance)= NONE y existe un usuario con ese username-> lanzar excepcion
+        if not self.instance and len(users) != 0:
+            raise serializers.ValidationError("Ya existe un usuario con ese username")
+        #Si estoy actualizando un usuario y el nombre de usuario ya existen
+        elif self.instance.username != data and len(users) != 0:
             raise serializers.ValidationError("Ya existe un usuario con ese username")
         else:
             return data
